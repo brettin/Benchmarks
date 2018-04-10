48f3f30b086f5e453b98f6ff5a4219aa8b23df69
brettin <brettin@anl.gov>
2018-04-10 12:05:14 +0000

    updatedf

--- Pilot1/Uno/uno_baseline_keras2_qsub.sh
+++ Pilot1/Uno/uno_baseline_keras2_qsub.sh
@@ -1,11 +1,11 @@
-OBALT -n 64 
+#!/bin/bash
+#COBALT -n 64 
 #COBALT -q default
-#COBALT -A Candle_ECP
-#COBALT -t 3:00:00
+#COBALT -A CSC249ADOA01 
+#COBALT -t 02:00:00
 
 set -x
 
-CONDA_ENV=tf-1.3.0_eigen_hv-0.9.6
 CONDA_ENV=py3.6_tf1.4
 
 source activate $CONDA_ENV
@@ -19,11 +19,12 @@ export OMP_NUM_THREADS=128
 export NUM_INTER_THREADS=1
 export NUM_INTRA_THREADS=128
 
-
+cache=$( hostname )
+cache=$COBALT_JOBID"_"$cache
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $DIR
 
-aprun -N 1 -cc none -b python $DIR/uno_baseline_keras2.py --cache cache.t
+aprun -N 1 -cc none -b python $DIR/uno_baseline_keras2.py --cache $cache"_cache" -v -l log.0 --use_landmark_genes
 
 source deactivate
