86b888363194d721a8a70905de5cae3073dd409a
brettin <brettin@anl.gov>
2018-08-16 14:29:06 -0500

    inferring only on test data

--- Pilot1/NT3/infer.py
+++ Pilot1/NT3/infer.py
@@ -101,29 +101,22 @@ print('output_dir is ', gParameters['output_dir'])
 
 # todo: enable user to specify csv file to load.
 def load_data(train_path, test_path, gParameters):
-    df_train = (pd.read_csv(train_path,header=None).values).astype('float32')
     df_test = (pd.read_csv(test_path,header=None).values).astype('float32')
 
-    print('df_train shape:', df_train.shape)
     print('df_test shape:', df_test.shape)
 
-    seqlen = df_train.shape[1]
+    seqlen = df_test.shape[1]
 
-    df_y_train = df_train[:,0].astype('int')
     df_y_test = df_test[:,0].astype('int')
 
-    Y_train = np_utils.to_categorical(df_y_train,gParameters['classes'])
     Y_test = np_utils.to_categorical(df_y_test,gParameters['classes'])
 
-    df_x_train = df_train[:, 1:seqlen].astype(np.float32)
     df_x_test = df_test[:, 1:seqlen].astype(np.float32)
 
-    X_train = df_x_train
     X_test = df_x_test
 
     scaler = MaxAbsScaler()
-    mat = np.concatenate((X_train, X_test), axis=0)
-    mat = scaler.fit_transform(mat)
+    mat = scaler.fit_transform(X_test)
 
     X_train = mat[:X_train.shape[0], :]
     X_test = mat[X_train.shape[0]:, :]
